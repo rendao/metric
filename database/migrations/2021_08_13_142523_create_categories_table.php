9481bbc268696e7aa2f513dd31a8714ae544376b
Metric <faly@qq.com>
2021-08-19 11:56:42 +0800

    Update 2021_08_13_142523_create_categories_table.php

--- database/migrations/2021_08_13_142523_create_categories_table.php
+++ database/migrations/2021_08_13_142523_create_categories_table.php
@@ -15,6 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->integer('position')->nullable();
             $table->string('name');
             $table->string('code')->unique();
             $table->string('slug')->unique();
