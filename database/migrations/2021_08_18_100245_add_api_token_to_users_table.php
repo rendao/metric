f1c0f0c0ed16f7788723aed41c2853214894bd76
Metric <faly@qq.com>
2021-08-20 10:26:29 +0800

    Revert "Update Migration"
    This reverts commit 7b3ab5b0abe9febd9e42ce4c598330c2287eed4f.
    
    e

---
+++ database/migrations/2021_08_18_100245_add_api_token_to_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApiTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('api_token', 80)->after('password')
+                                ->unique()
+                                ->nullable()
+                                ->default(null);
+            $table->dateTime('last_login_at')->before('created_at')
+            ->nullable()
+            ->default(null);
+
+            $table->string('last_login_ip')->before('created_at')
+            ->nullable()
+            ->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('api_token', 'last_login_at', 'last_login_ip');
+        });
+    }
+}
